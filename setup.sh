4f162baa79b38c141c6c285e4425c6945b16d6f6
Santiago García Rodríguez <133403031+theCodemander@users.noreply.github.com>
2023-07-15 22:47:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 
-#https://stackoverflow.com/     questions/6967331/how-do-i-install-a-script-to-run-anywhere-from-the-command-line
+#https://stackoverflow.com/questions/6967331/how-do-i-install-a-script-to-run-anywhere-from-the-command-line
 
 mkdir /opt/fwt \
         /opt/fwt/main \
-        /opt/fwt/files \
+        /opt/fwt/main/cache \
+        /opt/fwt/main/files \
+        /opt/fwt/main/templates \
         /opt/fwt/main/captures \
         /srv/http/fwtPage
 
 #cp ./scripts/fwt-1.1.5 /usr/local/bin
-
+cp -r ./scripts/templates /opt/fwt/main/files
+cp -r ./scripts/templates /opt/fwt/main/files
 
 pacman -S xterm aircrack-ng hostapd apache dnsmasq wireshark-cli wireshark-qt python python-pip --noconfirm 
 pacman -S python-click  --noconfirm
 
-usr=$(whoami)
-export PATH=$PATH:/usr/$usr/bin
+#usr=$(whoami)
+#export PATH=$PATH:/usr/$usr/bin
 
 echo -e "\033[32m Done!\033[37m - Now you can just type \033[31m sudo fwt -h \033[37m in the terminal to start the tool!"
