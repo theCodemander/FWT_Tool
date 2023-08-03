7fc2cc3b27d5fd56642c8859f62778d7b7ce96a6
Santiago García Rodríguez <133403031+theCodemander@users.noreply.github.com>
2023-08-03 13:19:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,12 @@ mkdir /opt/fwt \
         /opt/fwt/main/captures \
         /srv/http/fwtPage
 
-cp ./scripts/fwt-1.1.9 /usr/local/bin
-chmod +x /usr/local/bin/fwt-1.1.9
+cp ./scripts/fwt /usr/local/bin
+chmod +x /usr/local/bin/fwt
 
 cp -r ./scripts/templates /opt/fwt/main/
 
 pacman -S xterm aircrack-ng hostapd apache lighttpd iw dnsmasq wireshark-cli wireshark-qt python python-pip --noconfirm 
 pacman -S python-click --noconfirm
 
-echo -e "\033[32m Done!\033[37m - Now you can just type \033[31m sudo fwt -h \033[37m in the terminal to start the tool!"
+echo -e "\033[32m Done!\033[37m - Now you can just type \033[31m sudo fwt --help \033[37m in the terminal to start the tool!"
