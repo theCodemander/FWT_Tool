6944bab4b66ccb68a6640b7e2b876b2ee1286566
Santiago García Rodríguez <133403031+theCodemander@users.noreply.github.com>
2023-08-30 19:46:01 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+id -u &>/dev/null
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
 
+if [ -f "/usr/local/bin/fwt" ]
+then
+    rm /usr/local/bin/fwt -f
+    echo -e "FWT has been uninstalled from your system."
+else
+    echo -e "FWT is not present in your system."
+fi
